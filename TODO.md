3542566ff59232794e1830756d839e85aa9aa34a
yupengfei <yupengfei@foxmail.com>
2024-10-03 15:09:35 +0800

    1500到手

--- TODO.md
+++ TODO.md
@@ -1,4 +1,4 @@
 2024年10月3日
 
 1. 读xls数据
-2. [可视化](https://echarts.apache.org/examples/zh/editor.html?c=area-time-axis)
+2. [可视化](https://echarts.apache.org/examples/zh/editor.html?c=area-time-axis) [可视化2](https://echarts.apache.org/examples/zh/editor.html?c=doc-example%2Fcandlestick-axisPointer&edit=1&reset=1)
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,4 +1,9 @@
 <style scoped>
+#chart {
+  width: 100vw;
+  height: 90vh;
+}
+
 #upload {
   position: fixed;
   width: 360px;
@@ -21,18 +26,18 @@
     <label>{{ info }}</label>
   </div>
 
-  <div id="chart" style="width: 100%;height:100%;"></div>
+  <div id="chart"></div>
 
 </template>
 
 <script setup lang="ts">
 import { read, utils } from 'xlsx';
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
 import * as echarts from 'echarts';
-type EChartsOption = echarts.EChartsOption;
+// type EChartsOption = echarts.EChartsOption;
 
 type Point = {
-  记录时间: Date,
+  记录时间: string,
   温度1: number,
   温度2: number,
   温度3: number,
@@ -55,6 +60,7 @@ const onFileChanged = async (event: Event) => {
     let file = target.files[0];
     console.log(file)
     info.value = "正在加载" + file.name + "，需要十几秒"
+    blank()
     let workbook = read(await file.arrayBuffer(), { type: 'binary' })
     console.log(workbook.SheetNames[0])
     let first_ws = workbook.Sheets[workbook.SheetNames[0]];
@@ -62,10 +68,116 @@ const onFileChanged = async (event: Event) => {
     let data = utils.sheet_to_json<Point>(first_ws)
     console.log(data)
     info.value = "加载完成" + file.name
+    drawData(data)
   }
 }
 
-// const drawData(data) {
+onMounted(() => {
+  // drawData([])
+})
 
-// }
+const blank = () => {
+  var chartDom = document.getElementById('chart')!;
+  echarts.dispose(chartDom);
+
+}
+
+const drawData = (data: Point[]) => {
+  var chartDom = document.getElementById('chart')!;
+  var myChart = echarts.init(chartDom);
+  var option: echarts.EChartsOption;
+
+  option = {
+    xAxis: {
+      type: 'category',
+      boundaryGap: false,
+      data: data.map(d => d.记录时间)
+    },
+    yAxis: {
+      type: 'value'
+    },
+    tooltip: {
+      trigger: 'axis',
+      // axisPointer: {
+      //   type: 'cross'
+      // },
+      backgroundColor: 'rgba(255, 255, 255, 0.8)',
+      extraCssText: 'width: 200px'
+    },
+    dataZoom: [
+      {
+        type: 'inside',
+        start: 0,
+        end: 10
+      },
+      {
+        start: 0,
+        end: 10
+      }
+    ],
+    grid: {
+      left: "60px",
+      right: "60px",
+    },
+    // legend: {
+    //   type: 'scroll',
+    //   orient: 'vertical',
+    //   right: 10,
+    //   top: 20,
+    //   bottom: 20,
+    // },
+    legend: {
+      left: 'left'
+    },
+    series: [
+      {
+        name: "温度1",
+        type: 'line',
+        data: data.map(d => d.温度1)
+      },
+      {
+        name: "温度2",
+        type: 'line',
+        data: data.map(d => d.温度2)
+      },
+      {
+        name: "温度3",
+        type: 'line',
+        data: data.map(d => d.温度3)
+      },
+      {
+        name: "温度4",
+        type: 'line',
+        data: data.map(d => d.温度4)
+      },
+      {
+        name: "室内平均温度",
+        type: 'line',
+        data: data.map(d => d.室内平均温度)
+      },
+      {
+        name: "分区均温1",
+        type: 'line',
+        data: data.map(d => d.分区均温1)
+      },
+      {
+        name: "分区均温2",
+        type: 'line',
+        data: data.map(d => d.分区均温2)
+      },
+      {
+        name: "室外温度",
+        type: 'line',
+        data: data.map(d => d.室外温度)
+      },
+      {
+        name: "室外湿度",
+        type: 'line',
+        data: data.map(d => d.室外温度)
+      },
+    ]
+  };
+
+  option && myChart.setOption(option);
+}
 </script>
