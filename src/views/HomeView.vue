51efd16e4249a3a5677c6999adf8c8af0b00df68
yupengfei <yupengfei@foxmail.com>
2024-10-03 13:34:46 +0800

    准备绘图

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,11 @@
 <template>
-  <input type="file" @change="onFileChanged($event)"
-    accept="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" ref="file" />
+  <!-- <input type="file" @change="onFileChanged($event)"
+    accept="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" ref="file" /> -->
+  <input type="file" @change="onFileChanged($event)" accept="application/vnd.ms-excel" />
 </template>
 
 <script setup lang="ts">
-import { read } from 'xlsx';
-import { ref } from "vue"
-
-const file = ref(null)
+import { read, utils } from 'xlsx';
 
 // 异步读取，原理未知
 // https://stackoverflow.com/questions/39366810/how-can-i-read-a-file-from-an-html-input-with-js-xlsx
@@ -17,8 +15,13 @@ const onFileChanged = async (event: Event) => {
   if (target && target.files) {
     let file = target.files[0];
     console.log(file)
-    let wb = read(await file.arrayBuffer(), { type: 'binary' })
-    console.log(wb.SheetNames[0])
+    let workbook = read(await file.arrayBuffer(), { type: 'binary' })
+    console.log(workbook.SheetNames[0])
+    let first_ws = workbook.Sheets[workbook.SheetNames[0]];
+    // let aaa = first_ws[1]
+    // console.log(aaa)
+    let data = utils.sheet_to_json(first_ws)
+    console.log(data)
   }
 }
 </script>
