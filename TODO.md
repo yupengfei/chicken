8aba27c247b732758fc33480f211a59e7ec0c2bb
yupengfei <yupengfei@foxmail.com>
2024-10-03 13:22:04 +0800

    异步读取

---
+++ TODO.md
@@ -0,0 +1,4 @@
+2024年10月3日
+
+1. 读xls数据
+2. [可视化](https://echarts.apache.org/examples/zh/editor.html?c=area-time-axis)
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,24 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <input type="file" @change="onFileChanged($event)"
+    accept="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" ref="file" />
 </template>
+
+<script setup lang="ts">
+import { read } from 'xlsx';
+import { ref } from "vue"
+
+const file = ref(null)
+
+// 异步读取，原理未知
+// https://stackoverflow.com/questions/39366810/how-can-i-read-a-file-from-an-html-input-with-js-xlsx
+const onFileChanged = async (event: Event) => {
+
+  const target = event.target as HTMLInputElement;
+  if (target && target.files) {
+    let file = target.files[0];
+    console.log(file)
+    let wb = read(await file.arrayBuffer(), { type: 'binary' })
+    console.log(wb.SheetNames[0])
+  }
+}
+</script>
